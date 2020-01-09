82473ddedb7700576638cd4ea04bdd4939a812c6
w.mike.brichko@gmail.com <w.mike.brichko@gmail.com>
2020-01-09 12:20:22 -0500

    Added avigation between components

--- Recipe-Helper/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Recipe-Helper/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingedient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { read: true, static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { read: true, static: false }) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem(){
+    console.log(this.nameInputRef);
+    console.log(this.amountInputRef);
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.nameInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
