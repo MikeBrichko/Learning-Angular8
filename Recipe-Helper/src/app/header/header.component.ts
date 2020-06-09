649b2098b49866d29cb0215400558d1011a17a34
MikeBrichko <w.mike.brichko@gmail.com>
2020-06-09 12:25:44 -0400

    Authentication now works with ngrx

--- Recipe-Helper/src/app/header/header.component.ts
+++ Recipe-Helper/src/app/header/header.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import * as fromApp from '../store/app.reducer';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +15,17 @@ export class HeaderComponent {
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService, private authService : AuthService) {}
+  constructor(
+    private dataStorageService: DataStorageService, 
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe( user => {
+    this.userSub = this.store
+    .select('auth')
+    .pipe(map(authState => authState.user))
+    .subscribe( user => {
       this.isAuthenticated = !!user;
     });
   }
